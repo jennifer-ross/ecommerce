a71dac61cfe175b403894c70a63e6f36983b79eb
jennifer-ross <pguczol23@yandex.ru>
2023-09-05 15:30:36 +0500

    Update configuration

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -1,6 +1,17 @@
 import * as process from 'process'
 import * as path from 'path'
+import * as fs from 'fs'
 
 export default () => ({
     CLIENT_BUILD_PATH: path.join(process.cwd(), 'client'),
+    JWT_SECRET: fs.readFileSync(
+        path.join(process.cwd(), 'src/jwtRS256.key'),
+        'utf8',
+    ),
+    JWT_REFRESH_SECRET: fs.readFileSync(
+        path.join(process.cwd(), 'src/jwtRS256.key'),
+        'utf8',
+    ),
+    JWT_EXPIRES: '60s',
+    SITE_URL: 'my.site.com',
 })
