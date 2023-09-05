5f96497db87f4f1b08329815cb39d60e2406138c
jennifer-ross <pguczol23@yandex.ru>
2023-09-05 15:31:07 +0500

    Update main file

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,35 +8,74 @@ import { AppClusterService } from './services/app-cluster.service'
 import compression from '@fastify/compress'
 import secureSession from '@fastify/secure-session'
 import { eq, lowerCase } from 'lodash'
-import { NotepackPipe } from './pipes/notepack.pipe'
-import { FastifyRequest } from 'fastify'
+import fastifyCsrf from '@fastify/csrf-protection'
+import * as fs from 'fs'
+import * as path from 'path'
+import { DateTime } from 'luxon'
 
 async function bootstrap() {
+    // Initialisation
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter({ logger: true }),
+        {
+            rawBody: true,
+        },
     )
+
+    // Global Prefix
     await app.setGlobalPrefix('api')
 
-    await app.useGlobalPipes(new NotepackPipe())
+    // Interceptors
+    // await app.useGlobalInterceptors(new NotepackTransformInterceptor())
 
-    await app.enableCors()
-
+    // Configuration
+    await app.enableCors({
+        origin: ['http://localhost:5173', 'http://localhost'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    })
+    await app.register(fastifyCsrf)
     await app.register(compression, { encodings: ['gzip', 'deflate'] })
     await app.register(secureSession, {
-        secret: 'averylogphrasebiggerthanthirtytwochars',
+        secret: 'kRKXKuFMzsN7gZ0tBX1iQjRWQlTRB6PyqDdVNNM6aJ4',
         salt: 'mq9hDxBVDbspDR6n',
+        key: fs.readFileSync(path.join(__dirname, '../src/', 'secret-key')),
+        logLevel: 'trace',
+        cookie: {
+            path: '/',
+            expires: DateTime.now().plus({ day: 30 }).toBSON(),
+            domain: '127.0.0.1',
+            secure: true,
+            sameSite: 'none',
+            httpOnly: false,
+            // options for setCookie, see https://github.com/fastify/fastify-cookie
+        },
     })
 
-    await app.useBodyParser(
-        'application/x-www-form-urlencoded',
-        {},
-        (request: FastifyRequest) => {},
-    )
+    // BodyParsers
+    // const notePackParser = (request: FastifyRequest, rawBody: Buffer, done) => {
+    //     try {
+    //         const body = notepack.decode(rawBody) as object
+    //         request.body = body
+    //
+    //         done(null, body)
+    //     } catch (e) {
+    //         done(e)
+    //     }
+    // }
+    //
+    // await app.useBodyParser(
+    //     'application/x-www-form-urlencoded',
+    //     {},
+    //     notePackParser,
+    // )
 
+    // Start
     await app.listen(3000)
 }
 
+// Clusterize if enabled
 if (
     process.env.USE_CLUSTERS &&
     (eq(lowerCase(process.env.USE_CLUSTERS), 'true') ||
